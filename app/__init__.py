b6d89a4ef56058bbcafec0c443d96b33c473178b
PureJZ <jzeng246@gmail.com>
2024-12-04 11:05:02 -0500

    Initiate db

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,16 @@
-import Flask from flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import sqlite3
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from build_db import setup_database
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+def get_db_connection():
+    conn = sqlite3.connect('stonks.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def home():
