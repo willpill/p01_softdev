06412515fbd7f17e5a49c9ae0b2260c1d51c9994
Jackie Zeng <jzeng246@gmail.com>
2024-12-15 16:24:37 -0500

    updated currency exchange so round to nearest thousandth and add dropdown for currency selection

--- app/__init__.py
+++ app/__init__.py
@@ -74,20 +74,76 @@ def register():
         finally:
             conn.close()
     return render_template('register.html')
-
+CURRENCY_OPTIONS = """
+<option value='AED' title='United Arab Emirates Dirham'>AED</option>
+<option value='AFN' title='Afghan Afghani'>AFN</option>
+<option value='ALL' title='Albanian Lek'>ALL</option>
+<option value='AMD' title='Armenian Dram'>AMD</option>
+<option value='ANG' title='Netherlands Antillean Guilder'>ANG</option>
+<option value='AOA' title='Angolan Kwanza'>AOA</option>
+<option value='ARS' title='Argentine Peso'>ARS</option>
+<option value='AUD' title='Australian Dollar'>AUD</option>
+<option value='AWG' title='Aruban Florin'>AWG</option>
+<option value='AZN' title='Azerbaijani Manat'>AZN</option>
+<option value='BAM' title='Bosnia-Herzegovina Convertible Mark'>BAM</option>
+<option value='BBD' title='Barbadian Dollar'>BBD</option>
+<option value='BDT' title='Bangladeshi Taka'>BDT</option>
+<option value='BGN' title='Bulgarian Lev'>BGN</option>
+<option value='BHD' title='Bahraini Dinar'>BHD</option>
+<option value='BIF' title='Burundian Franc'>BIF</option>
+<option value='BMD' title='Bermudan Dollar'>BMD</option>
+<option value='BND' title='Brunei Dollar'>BND</option>
+<option value='BOB' title='Bolivian Boliviano'>BOB</option>
+<option value='BRL' title='Brazilian Real'>BRL</option>
+<option value='BSD' title='Bahamian Dollar'>BSD</option>
+<option value='BTC' title='Bitcoin'>BTC</option>
+<option value='BTN' title='Bhutanese Ngultrum'>BTN</option>
+<option value='BWP' title='Botswanan Pula'>BWP</option>
+<option value='BYN' title='Belarusian Ruble'>BYN</option>
+<option value='BZD' title='Belize Dollar'>BZD</option>
+<option value='CAD' title='Canadian Dollar'>CAD</option>
+<option value='CHF' title='Swiss Franc'>CHF</option>
+<option value='CNY' title='Chinese Yuan'>CNY</option>
+<option value='COP' title='Colombian Peso'>COP</option>
+<option value='CRC' title='Costa Rican Colón'>CRC</option>
+<option value='CUP' title='Cuban Peso'>CUP</option>
+<option value='CZK' title='Czech Republic Koruna'>CZK</option>
+<option value='DKK' title='Danish Krone'>DKK</option>
+<option value='DOP' title='Dominican Peso'>DOP</option>
+<option value='EGP' title='Egyptian Pound'>EGP</option>
+<option value='EUR' title='Euro'>EUR</option>
+<option value='GBP' title='British Pound Sterling'>GBP</option>
+<option value='HKD' title='Hong Kong Dollar'>HKD</option>
+<option value='INR' title='Indian Rupee'>INR</option>
+<option value='JPY' title='Japanese Yen'>JPY</option>
+<option value='KES' title='Kenyan Shilling'>KES</option>
+<option value='KRW' title='South Korean Won'>KRW</option>
+<option value='MXN' title='Mexican Peso'>MXN</option>
+<option value='NGN' title='Nigerian Naira'>NGN</option>
+<option value='NOK' title='Norwegian Krone'>NOK</option>
+<option value='NZD' title='New Zealand Dollar'>NZD</option>
+<option value='USD' title='United States Dollar'>USD</option>
+<option value='ZAR' title='South African Rand'>ZAR</option>
+"""
 @app.route('/currency_exchange', methods=['GET', 'POST'])
 def currency_exchange():
     conversion_result = None
+
     if request.method == 'POST':
-        base_currency = request.form['base_currency']
-        target_currency = request.form['target_currency']
-        amount = float(request.form['amount'])
-        conversion_result = money.convert_currency(amount, base_currency, target_currency)
-        conversion_result = str(conversion_result)
-        index = conversion_result.find(".")
-        if index != -1:
-            conversion_result = conversion_result[:index + 4]
-    return render_template('currency_exchange.html', result=conversion_result)
+        try:
+            base_currency = request.form['base_currency']
+            target_currency = request.form['target_currency']
+            amount = float(request.form['amount'])
+            
+            # Perform the currency conversion
+            conversion_result = money.convert_currency(amount, base_currency, target_currency)
+            
+            # Round to the nearest thousandth (3 decimal places)
+            conversion_result = round(conversion_result, 3)
+        except (ValueError, TypeError):
+            conversion_result = "Invalid input or conversion error"
+
+    return render_template('currency_exchange.html', result=conversion_result, dropdown_options=CURRENCY_OPTIONS)
 
 if __name__ == '__main__':
     setup_database()
